ccea4b6dbf48764080840bccec3c9bf8bbf6fcd8
Yoga Ganesha <yganesha@cloudbees.com>
2023-08-12 22:57:35 -0700

    add nats test code

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+
+	messaging "github.com/calculi-corp/common/pkg/messaging"
+	"github.com/calculi-corp/common/pkg/messaging/subjects"
+	"github.com/calculi-corp/config"
+	testutil "github.com/calculi-corp/grpc-testutil"
+	"github.com/calculi-corp/log"
+	nats "github.com/calculi-corp/nats"
+	"github.com/calculi-corp/nats/testdata"
 )
 
 func main() {
 	fmt.Println("This is experimental repo, have fun!")
+	MyNats1()
 	for {
 	}
 }
@@ -20,4 +31,44 @@ func forTest(a, b int) int {
 	fmt.Println("Testing GX-4884")
 	fmt.Println("Testing GX-4884")
 	return r
+}
+
+type awscreds struct {
+	Access   string `json:"access"`
+	Secret   string `json:"secret"`
+	IAMRole  string `json:"iam_role"`
+	Password string `json:"password"`
+}
+
+func VStrings() {
+	creds := &awscreds{
+		Access:   "somekey",
+		Secret:   "secret",
+		Password: "myawesomepassword",
+	}
+
+	log.Infof("creds: %v", creds)
+}
+
+func MyNats1() {
+	testutil.SetUnitTestConfig()
+	config.Config.SetCliFlags()
+	config.Config.Set("nats.server", "tls://localhost:4222")
+	log.Info("Starting NATS test")
+	natsClient, err := nats.NewMessagingClient()
+	if log.CheckErrorf(err, "Failed to create NATS client") {
+		os.Exit(1)
+	}
+	natsClient.Subscribe(subjects.HttpEvent_InstrumentationUI, myTestHander)
+}
+
+func myTestHander(msg messaging.Message) error {
+	log.Infof("raw message %s", msg)
+	tstMsg := &testdata.TestMessage{}
+	err := json.Unmarshal(msg.Data(), tstMsg)
+	if log.CheckErrorf(err, "Failed to unmarshal message") {
+		return err
+	}
+	log.Infof("Received message %s", tstMsg)
+	return nil
 }
--- main_test.go
+++ main_test.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/calculi-corp/common/pkg/messaging/subjects"
+	"github.com/calculi-corp/config"
+	testutil "github.com/calculi-corp/grpc-testutil"
+	nats "github.com/calculi-corp/nats"
+	"github.com/calculi-corp/nats/testdata"
 )
 
 func TestForTest(t *testing.T) {
@@ -16,4 +25,18 @@ func TestForTest2(t *testing.T) {
 	if r != 10 {
 		t.Error("expected return value of 100, got ", r)
 	}
+}
+
+func TestPublish(t *testing.T) {
+	testutil.SetUnitTestConfig()
+	config.Config.SetCliFlags()
+	config.Config.Set("nats.server", "tls://localhost:4222")
+
+	natsClient, err := nats.NewMessagingClient()
+	require.NoError(t, err)
+	require.NotNil(t, natsClient)
+	tstMsg := &testdata.TestMessage{Name: "myTestMessage", Value: "myAwesomeValue"}
+	tb, err := json.Marshal(tstMsg)
+	require.NoError(t, err)
+	natsClient.Publish(subjects.HttpEvent_InstrumentationUI, tb)
 }
